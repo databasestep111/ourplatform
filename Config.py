e2b4e19065b34f8dc15c5d19b179e28c7c28e77f
databasestep111 <krishjay379@gmail.com>
2026-08-23 16:11:49 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,14 @@
 APP_NAME = "OurPlatform"
-VERSION = "v0.001"
+VERSION = "v0.005"
+
 DEBUG = True
+
+DEFAULT_LANGUAGE = "en"
+
+FEATURES = {
+    "ai": True,
+    "memory": True,
+    "search": True,
+    "research": True,
+    "verification": False,
+}
